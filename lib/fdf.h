d3e4bb1f0a1901cebb453176fdf841d94e35f1b1
Thang Bui Quoc <tbui-quo@student.42wolfsburg.de>
2023-12-15 17:52:25 +0100

    some test function to see how minilibx works

--- lib/fdf.h
+++ lib/fdf.h
@@ -21,4 +21,13 @@
 # include <unistd.h>
 # include <stdio.h>
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+
 #endif
