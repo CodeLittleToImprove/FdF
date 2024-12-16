d065acf758faa23a2b42f4caf1965cac815f4f7a
Thang Bui Quoc <tbuiloves42@gmail.com>
2024-12-16 14:29:56 +0100

    added new target for clion to test minilibx

---
+++ lib/mlx_test.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui-quo <tbui-quo@student.42wolfsburg.d>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 18:05:49 by tbui-quo          #+#    #+#             */
+/*   Updated: 2024/02/27 18:05:49 by tbui-quo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MLX_TEST_H
+# define MLX_TEST_H
+
+# include "../lib/mlx_linux/mlx.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
+
+typedef struct s_img
+{
+	void	*img_ptr;
+	char	*img_pixels_ptr;
+	int		bits_per_pixel;
+	int		endian;
+	int		line_len;
+}				t_img;
+
+
+#endif
--- src/test_minilibx.c
+++ src/test_minilibx.c
@@ -25,13 +25,6 @@
 //
 //	return (0);
 //}
-//void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-//{
-//	char	*dst;
-//
-//	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-//	*(unsigned int*)dst = color;
-//}
 
 //void my_mlx_pixel_put(t_data *data, int x, int y, int size, int color)
 //{
@@ -47,36 +40,59 @@
 //	}
 //}
 
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+{
+	char	*dst;
 
-//int main(void)
-//{
-//	void *mlx;
-//	void *mlx_win;
-//	t_data img;
-//
-//	// Initialize the MiniLibX
-//	mlx = mlx_init();
-//
-//	// Create a window
-//	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-//
-//	// Create an image
-//	img.img = mlx_new_image(mlx, 1920, 1080);
-//
-//	// Get image information
-//	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-//
-//	// Put a red pixel at coordinates (5, 5)
-//	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-//
-//	//make a line
+	dst = img->img_pixels_ptr + (y * img->line_len + x * (img->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+
+int main(void)
+{
+	void *mlx;
+	void *mlx_win;
+	t_img img;
+
+	// Initialize the MiniLibX
+	mlx = mlx_init();
+
+	// Create a window
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+
+	// Create an image
+	img.img_ptr = mlx_new_image(mlx, 1920, 1080);
+
+	// Get image information
+	img.img_pixels_ptr = mlx_get_data_addr(img.img_ptr, &img.bits_per_pixel, &img.line_len, &img.endian);
+
+	// Put a red pixel at coordinates (5, 5)
+	my_mlx_pixel_put(&img, 50, 50, 0x00FF0000);
+
+//	//make a horizontal line
 //	for (int i = 0; i < 1920; ++i)
 //	{
 //		my_mlx_pixel_put(&img,0 + i, 0, 0x00FF0000);
 //	}
-//	// Put the image on the window
-//	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-//
-//	// Enter the MiniLibX event loop
-//	mlx_loop(mlx);
-//}
+//	// make vertical line
+//	for (int i = 0; i < 1080; ++i)
+//	{
+//		my_mlx_pixel_put(&img,0, 0 + i, 0x00FF0000);
+//	}
+	for (int i = 0; i < 1920; ++i)
+	{
+		int j = 0; // Reset `j` for each column
+		while (j < 1080)
+		{
+			my_mlx_pixel_put(&img, i, j, 0x00FF0000); // Paint pixel (i, j) red
+			++j;
+		}
+	}
+
+	// Put the image on the window
+	mlx_put_image_to_window(mlx, mlx_win, img.img_ptr, 0, 0);
+
+	// Enter the MiniLibX event loop
+	mlx_loop(mlx);
+}
